dfd76d8d56b894da813d8fc1ccd355af9c891d0c
nhlinh2207 <nguyenhoailinh2207@gmail.com>
2024-04-19 16:27:07 +0700

    19/04/2024 - Add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk:8
+ARG JAR_fILE=target/*.jar
+COPY ${JAR_fILE} freshfoodBackend.jar
+ENTRYPOINT ["java", "-jar", "/freshfoodBackend.jar"]
+EXPOSE 8080
+
+# Build jar : mvn clean package -DskipTests=true
+# docker build -t nhlinh2207/freshfoodbackend:1 .
+# docker run -p 8080:8080 -e DB_HOST=192.168.25.129 -e DB_USER=root -e DB_PASS=root freshfoodbackend:1
--- src/main/java/com/linh/freshfoodbackend/repository/IAddressRepo.java
+++ src/main/java/com/linh/freshfoodbackend/repository/IAddressRepo.java
@@ -2,6 +2,7 @@ package com.linh.freshfoodbackend.repository;
 
 import com.linh.freshfoodbackend.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface IAddressRepo extends JpaRepository<Address, Integer> {
+public interface IAddressRepo extends JpaSpecificationExecutor<Address>, JpaRepository<Address, Integer> {
 }
